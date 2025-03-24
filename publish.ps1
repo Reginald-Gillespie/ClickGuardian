11bec29c980999c9eb69d2a4eb9fa9fdb6dd6723
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2025-03-24 15:34:40 -0500

    Settings

---
+++ publish.ps1
@@ -0,0 +1,3 @@
+dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugType=None -p:DebugSymbols=false -o ./build
+
+Compress-Archive -Path ./build/* -DestinationPath ClickGuardian.zip
